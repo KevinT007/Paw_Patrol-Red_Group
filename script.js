fe291bcbbfd3e8c2565d7c014931156279747108
Kevin T <112099258+KevinT007@users.noreply.github.com>
2022-09-19 03:33:49 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//* adds & removes responsive class to/from top nav when icon is clicked. */
-function myFunction() {
-    var x = document.getElementById("topnavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
