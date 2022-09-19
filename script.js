32665a9aa00116421beea173a74023e96b70101e
Kevin T <112099258+KevinT007@users.noreply.github.com>
2022-09-19 03:35:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//* adds & removes responsive class to/from top nav when icon is clicked. */
+function myFunction() {
+    var x = document.getElementById("topnavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
